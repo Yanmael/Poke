12f36c754009aa3a0f721af3e3fd40bb19dbc7e9
Deadpool <mcoc2657@gmail.com>
2026-04-12 19:48:41 -0400

    commit inicial

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(contexto) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = contexto.renderPage;
+
+    try {
+      contexto.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(contexto);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
